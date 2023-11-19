11ee5ac80415eb8f2eb585b0eb2a249998c03fae
Elliott Cost <email@elliott.computer>
2023-11-19 15:24:41 +0200

    website

---
+++ script.js
@@ -0,0 +1,59 @@
+document.getElementById('toggle-labels').addEventListener('change', function(e) {
+    var categories = document.querySelectorAll('[id^="category"]');
+
+    categories.forEach(function(category) {
+        if (e.target.checked) {
+            // Fade in
+            category.style.opacity = 0;
+            category.style.display = 'block';
+            setTimeout(function() { 
+                category.style.opacity = 1;
+            }, 10); // Delay needed for the browser to register 'display' change
+        } else {
+            // Fade out
+            category.style.opacity = 0;
+            setTimeout(function() { 
+                category.style.display = 'none'; 
+            }, 500); // Delay should match the CSS transition duration
+        }
+    });
+});
+
+
+window.onload = function() {
+    const svgElement = document.querySelector('svg');
+
+    if (svgElement) {
+        const svgBounds = svgElement.getBoundingClientRect();
+
+        const verticalScrollPosition = window.pageYOffset + svgBounds.top + (svgBounds.height / 2) - (window.innerHeight / 2);
+        const horizontalScrollPosition = window.pageXOffset + svgBounds.left + (svgBounds.width / 2) - (window.innerWidth / 2);
+
+        window.scrollTo({
+            top: verticalScrollPosition,
+            left: horizontalScrollPosition,
+            behavior: 'smooth'
+        });
+    }
+};
+
+document.getElementById('randomButton').addEventListener('click', function() {
+    const svgLinks = document.querySelectorAll('svg a');
+    if (svgLinks.length === 0) return; // Check if there are any links
+
+    const randomIndex = Math.floor(Math.random() * svgLinks.length); // Get a random index
+    const randomLink = svgLinks[randomIndex];
+
+    // Correctly accessing the href attribute for SVG elements
+    const href = randomLink.href.baseVal;
+
+    // Navigate to the link's href or perform any action required
+    window.location.href = href;
+});
+
+
+
+
+
+
+
